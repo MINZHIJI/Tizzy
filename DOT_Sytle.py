5ab81ffef3ec7b0854add820743633e9a90cd0ea
Ezither <asdjkl811114@gmail.com>
2021-01-10 23:26:33 +0800

    Collector will be dictionary type

--- DOT_Sytle.py
+++ DOT_Sytle.py
@@ -51,10 +51,6 @@ class Format_Dot_File():
             r'(^)?[^\S\n]*/(?:\*(.*?)\*/[^\S\n]*|/[^\n]*)($)?',
             re.DOTALL | re.MULTILINE
         )
-        # self.__del_cmt_re = re.compile(
-        #     r'/\*[\s\S]*?\*/',
-        #     re.DOTALL | re.MULTILINE
-        # )
         
         self.__target_dotfile = self.__del_cmt_re.sub(comment_replacer, self.__source_dotfile)
         return self.__target_dotfile
@@ -81,29 +77,55 @@ class Report_Dot_File():
         RAM_Set_Test_Value
         // STATE_END
         Description:
-        State_Collector will create RAM_DU type and record
-        the item in this type
+        State_Collector will create RAM_DU list and record
+        the item in this group
         """
         print("Enter State_Collector")
         re_state_start = re.compile(r'([ \t]*//[ \t]*STATE_START[ \t]*(\w+))')
         re_state_end = re.compile(r'([ \t]*//[ \t]*STATE_END*)')
         file = self.__source_dotfile.split('\n')
         Declare_Flag = False
-        state_type = ""
+        state_group = []
         state_list = []
+        stat_dirt = {}
+        state_list_index = 0
+        # for line in file:
+        #     m = re_state_start.search(line)
+        #     if(m is not None):
+        #         Declare_Flag = True
+        #         state_group.append()
+        #         state_list.append([])
+        #         continue
+        #     m = re_state_end.search(line)
+        #     if(m is not None):
+        #         Declare_Flag = False
+        #         state_list_index = state_list_index + 1
+        #         continue
+        #     if(Declare_Flag):
+        #         state_list[state_list_index].append(line)
+        # for group in state_list:
+        #     tmp_index = state_list.index(group)
+        #     print("[%s]"%state_group[tmp_index])
+        #     for entry in group:
+        #         print (entry)
+        #     print("------")
         for line in file:
             m = re_state_start.search(line)
             if(m is not None):
+                group_name = m.group(2)
                 Declare_Flag = True
-                state_type = m.group(2)
-                print(state_type)
+                if group_name not in stat_dirt:
+                    stat_dirt[group_name] = list()
                 continue
             m = re_state_end.search(line)
             if(m is not None):
                 Declare_Flag = False
                 continue
             if(Declare_Flag):
-                state_list.append(line)
-
-        for entry in state_list:
-            print(entry)
+                if group_name in stat_dirt:
+                    stat_dirt[group_name].append(line)
+        for state_group, state_list in stat_dirt.items():
+            print("[%s]"%state_group)
+            for entry in state_list:
+                print (entry)
+            print("------")
